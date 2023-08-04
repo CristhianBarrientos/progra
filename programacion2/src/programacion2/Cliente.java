3224fa490cbcddda763174c600434ffb1024c6d2
CristhianBarrientos <109832654+CristhianBarrientos@users.noreply.github.com>
2023-08-03 23:03:15 -0600

    Add files via upload

---
+++ programacion2/src/programacion2/Cliente.java
@@ -0,0 +1,48 @@
+package programacion2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Cliente extends Persona {
+
+    private String nit;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nit, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.nit = nit;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    @Override
+    protected String[] crear() {
+        try {
+            String datos[] = new String[6];
+            datos[0] = getNit();
+            datos[1] = getNombres();
+            datos[2] = getApellidos();
+            datos[3] = getDireccion();
+            datos[4] = getTelefono();
+            datos[5] = getFecha_Nacimiento();
+            return datos;
+        } catch (Exception ex) {
+
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
+
+    }
+
+}
